4224ce718878513d66c7b4ed8df9d19183c1126e
Meguelzz <meguelzz.oliveira@gmail.com>
2025-04-02 21:43:58 -0300

    Add files via upload

---
+++ AtvJS1/aluno.js
@@ -0,0 +1,14 @@
+import { pessoa } from  "./pessoa.js";
+
+
+class aluno extends pessoa {
+    constructor(nome,idade) {
+        super(nome,idade);
+        this.nome = nome;
+        this.idade = idade;}
+
+     falar() {
+        return "Perguntando..."}
+}
+
+export { aluno };
---
+++ AtvJS1/professor.js
@@ -0,0 +1,14 @@
+import { pessoa } from  "./pessoa.js";
+
+class professor extends pessoa {
+    constructor(nome,idade) {
+        super(nome,idade);
+        this.nome = nome;
+        this.idade = idade;
+    }
+     falar() {
+        return "Dando Aula..."
+     }
+}
+
+export { professor };
